2bd2f42d1d4000a1330f5d711161a6f13969edde
小島　崇彦 <taka1192ka@outlook.com>
2025-05-29 00:42:48 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
-├ app.py
-├ requirements.txt
-└ その他画像やロゴなど
+import streamlit as st
+
+st.set_page_config(
+    page_title="賭神AI競馬予想",
+    page_icon="🐎",
+    layout="centered",
+    initial_sidebar_state="collapsed"
+)
+
+st.title("🐎 賭神 AI競馬予想")
+st.write("ようこそ！こちらは賭神AIによる競馬予想サイトです。")
